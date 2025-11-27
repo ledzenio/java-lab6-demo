5699637807ea461ea7c3ff4d7a8b31bf550a7ce5
Eugene <146964188+ledzenio@users.noreply.github.com>
2025-11-28 01:52:18 +0300

    Create Task.java

---
+++ src/main/java/com/store/Task.java
@@ -0,0 +1,46 @@
+package com.store;
+
+/**
+ * Класс задачи
+ */
+public class Task {
+    private int id;
+    private String description;
+    private boolean completed;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        String status = completed ? "DONE" : "TODO";
+        return String.format("#%d [%s] %s", id, status, description);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Task task = (Task) obj;
+        return id == task.id;
+    }
+}
